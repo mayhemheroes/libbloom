5f71e7d9257a513c91292fd435c4f43861e608f8
jaketbrown <jaketbrown1119@gmail.com>
2022-10-16 19:27:01 -0400

    Initial Mayhem integration

---
+++ mayhem/fuzzer.c
@@ -0,0 +1,20 @@
+#include "../bloom.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <math.h>
+
+
+int LLVMFuzzerTestOneInput(uint8_t *data, size_t size) {
+    struct bloom bloom;
+    int status = bloom_init2(&bloom, 1000000, 0.01);
+    //bloom_add(&bloom, buffer, buflen);
+    printf("============\n");
+    int s_data = (size_t) data;
+    
+   // if (bloom_check(&bloom, buffer, buflen)) {
+   //     printf("It may be there!\n");
+   // }	
+   return 0;
+    
+}
