4c6977487c0f5b0789e758db66be0f7449966a55
jaketbrown <jaketbrown1119@gmail.com>
2022-10-21 21:59:55 -0400

    Update Mayhemfile and fix dockerfile

--- mayhem/basic-fuzz.c
+++ mayhem/basic-fuzz.c
@@ -26,8 +26,33 @@
 
 int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
 {
+  printf("=== Entering...\n");
   struct bloom bloom;
 
+  uint8_t copy[Size];
+  memcpy(copy, Data, Size); // make a non-const copy of the fuzz data
+    
+  uint8_t val = 0;
+  for (int i = 0; i < Size; i++) {
+    if (copy[i] != 0) {
+    	val = copy[i];
+    	break;
+    }
+  }
+  
+  unsigned int entries = val;
 
+  bloom_init2(&bloom, entries, 0.01);
+    
+  for (unsigned int n = 1; n < entries; n++) {
+    bloom_add(&bloom, &n, sizeof(unsigned int));
+  }
+
+  if (bloom_check(&bloom, Data, Size)) { 
+    printf("It may be there!\n");
+    bloom_print(&bloom);
+  }
+  bloom_free(&bloom);
+  printf("Done!\n");
   return 0;
 }
